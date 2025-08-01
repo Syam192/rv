231d8dda3cfbc69482092a54d8bb19ddb428a11a
Syam192 <masasyam41@gmail.com>
2025-08-02 01:02:41 +0700

    Delete preload.js

--- preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    sendWindowControl: (command) => ipcRenderer.send('window-control', command)
-});
